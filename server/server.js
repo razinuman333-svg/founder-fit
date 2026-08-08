92fac13febcb4808f196f5840257f331f4f50577
Numan Razi <razinuman333@gmail.com>
2026-08-08 16:18:57 +0400

    Initialize server with environment configuration and database connection; set up JSON parsing middleware.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
+import 'dotenv/config'
 import express from 'express'
-
-
+import connectDB from './config/db.js' 
 
 
 const app = express()
 const PORT = 3000
+
+
+await connectDB()
+
+
+app.use(express.json())
 
 
 app.get('/',(req,res)=>{
