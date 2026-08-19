06626a8d5739f6933a43c6cb1d90a84bd325c625
Numan Razi <razinuman333@gmail.com>
2026-08-19 16:18:25 +0400

    Add user authentication handling in Navbar; display UserButton if logged in

--- client/src/componts/Navbar.jsx
+++ client/src/componts/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from '../assets/founderFit-logo.png'
 import { Compass, Heart, MessageSquare, Users } from 'lucide-react'
-import { useClerk } from '@clerk/clerk-react'
+import { useClerk,useUser,UserButton } from '@clerk/clerk-react'
 
 function Navbar() {
   const { openSignIn } = useClerk()
+  const { user } = useUser()
 
   const navItems = [
     { label: 'Discover', icon: Compass, active: true },
@@ -30,12 +31,16 @@ function Navbar() {
             <li className='cursor-pointer hover:text-primary transition-colors'>Network</li>
           </ul>
 
+        {!user ? (
           <button
             onClick={openSignIn}
             className='bg-primary text-white px-4 py-1.5 sm:px-7 sm:py-2 rounded-full text-sm sm:text-base font-medium cursor-pointer shadow-lg shadow-primary/30 shrink-0 transition-all duration-300 hover:scale-105 hover:shadow-xl hover:shadow-primary/40 hover:brightness-110 active:scale-95'
           >
             Login
           </button>
+        ) : (
+          <UserButton />
+        )}
         </div>
       </header>
 
